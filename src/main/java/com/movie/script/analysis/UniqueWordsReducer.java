92088bb569c95df17d298af7c8741e02a7b0a6d7
narayanadattanishith <143428446+narayanadattanishith@users.noreply.github.com>
2025-02-11 22:03:03 +0000

    My Handson4 Output

--- src/main/java/com/movie/script/analysis/UniqueWordsReducer.java
+++ src/main/java/com/movie/script/analysis/UniqueWordsReducer.java
@@ -1,5 +1,4 @@
 package com.movie.script.analysis;
-
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -10,6 +9,10 @@ public class UniqueWordsReducer extends Reducer<Text, Text, Text, Text> {
 
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-
+        HashSet<String> uniqueWords = new HashSet<>();
+        for (Text val : values) {
+            uniqueWords.add(val.toString());
+        }
+        context.write(key, new Text(uniqueWords.toString()));
     }
 }
